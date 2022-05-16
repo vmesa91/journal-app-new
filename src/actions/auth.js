26dabdb3eb8aed719a4adcf3c2d21d48c2e0f622
Virginia Mesa <virginiamesa@packlink.com>
2022-05-16 12:08:49 +0200

    Configure firebase

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,6 @@
-import { types } from "../types/types"
 
+import { types } from "../types/types"
+import { firebase, googleAuthProvider } from "../firebase/firebase-config";
 /* export const login = (uid, displayName) => {
     return {
         type: types.login,
@@ -9,6 +10,27 @@ import { types } from "../types/types"
         }
     }
 }  ES LO MISMO QUE : (Un único argumento el return) */
+
+
+export const startLoginEmailPassword = (email, password) => {
+    return (dispatch) => {
+        setTimeout(() => {
+            dispatch( login(123, 'Pedro') )
+        }, 3500);
+    }
+}
+
+export const startGoogleLogin = () => {
+    return (dispatch) => {
+        firebase.auth().signInWithPopup( googleAuthProvider )
+            .then( ( { user } ) => {
+                console.log(user.email)
+                console.log(user.uid)
+                console.log(user.displayName)
+                dispatch(login( user.uid, user.displayName ))
+            } ) 
+    }
+}
 
 export const login = (uid, displayName) => 
 (     {
