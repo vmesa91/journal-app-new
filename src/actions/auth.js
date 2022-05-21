571dee6ad4a1ae7a0287c02130e8678e843eb3d1
Virginia Mesa <virginiamesa@packlink.com>
2022-05-21 11:10:03 +0200

    Register complete

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -32,6 +32,19 @@ export const startGoogleLogin = () => {
     }
 }
 
+export const startRegisterWithEmailPasswordName = ( email, password, name) => {
+    return ( dispatch ) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then( async( { user } ) => {
+            await user.updateProfile( { displayName: name } )
+            dispatch(login( user.uid, user.displayName ))
+        } )
+        .catch( e => {
+            console.log(e)
+        }) 
+    }
+}
+
 export const login = (uid, displayName) => 
 (     {
         type: types.login,
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -3,22 +3,16 @@ import { Link } from 'react-router-dom'
 import useForm from '../../hooks/useForm'
 import validator from "validator";
 import { setError, removeError } from '../../actions/ui';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { startRegisterWithEmailPasswordName } from '../../actions/auth';
 
 export const RegisterScreen = () => {
 
 
-  /* 
-    {
-      name: 'Virginia',
-      surname: 'Mesa',
-      email: 'v.mesagetafe@gmail.com',
-      password: '12345',
-      password: '12345'
-    }
-  */
-  
   const dispatch = useDispatch()
+
+  const { msgError } = useSelector( state => state.ui )
+
 
   const [formValues, handleInputChange] = useForm({
    name: 'Virginia',
@@ -34,7 +28,7 @@ export const RegisterScreen = () => {
     e.preventDefault()
     
     if (isFormValid()) {
-        console.log('Fork OK')
+        dispatch (startRegisterWithEmailPasswordName(email, password, name) )
     }
   }
 
@@ -57,10 +51,13 @@ export const RegisterScreen = () => {
 
   return (
     <>
-       <h3 className='auth__title'>Regsiter</h3>
+       <h3 className='auth__title'>Register</h3>
        <form onSubmit={handleRegister}>
 
-          <div className='auth__alert-error'> ERROR </div>
+         { 
+              msgError && (<div className='auth__alert-error'> {msgError} </div>)
+         
+         }
           <input
           type='text'
           placeholder='Name'
