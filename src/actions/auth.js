653d9b64c02516a524273bd52310d803f529c778
Virginia Mesa <virginiamesa@packlink.com>
2022-05-21 18:56:29 +0200

    Login complete

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,6 @@
 
 import { types } from "../types/types"
+import Swal from 'sweetalert2'
 import { firebase, googleAuthProvider } from "../firebase/firebase-config";
 import { finishLoading, startLoading } from "./ui";
 
@@ -7,6 +8,7 @@ import { finishLoading, startLoading } from "./ui";
 export const startLoginEmailPassword = (email, password) => {
     return (dispatch) => {
         dispatch ( startLoading() )
+
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then( ( { user } ) => {
             dispatch( login(user.uid, user.displayName) )
@@ -15,6 +17,7 @@ export const startLoginEmailPassword = (email, password) => {
         .catch( e => {
             console.log(e)
             dispatch ( finishLoading() )
+            Swal.fire('Error', e.message, 'error')
         }) 
     }
 }
@@ -40,6 +43,7 @@ export const startRegisterWithEmailPasswordName = ( email, password, name) => {
         } )
         .catch( e => {
             console.log(e)
+            Swal.fire('Error', e.message, 'error')
         }) 
     }
 }
